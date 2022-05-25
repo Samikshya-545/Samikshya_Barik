685399e919360d7dcefcc066cf381f0802b4bd13
unknown <codesamiskshya123@gmail.com>
2022-05-25 16:11:34 +0530

    solved Problem 1

---
+++ problem1/problem1.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route('/numbers',methods = ['GET'])
+def login():
+   if request.method == 'GET':
+
+        params = request.args.to_dict(flat=False)
+        allIpNumbers = []
+        for param in params.get('url'):
+            temp = json.loads(requests.get(param).text)
+            print()
+            allIpNumbers = allIpNumbers + temp['numbers']
+        
+        allIpNumbers.sort()
+        res = []
+        print(allIpNumbers)
+        for i in allIpNumbers:
+            if i not in res:
+                res.append(i)
+        
+        print(res)
+
+        return {"numbers": res}
+
+if __name__ == '__main__':
+   app.run(debug = True)
